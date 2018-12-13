65e5508321b745f2a1647cbc7e93707290ed911b
rkishir <rkapoor@ishir.com>
2018-12-13 12:55:02 +0530

    Make component data dynamic using state object.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import HelloUser from "./HelloUser";
 
 class App extends Component {
+  state = {
+    firstUsername: "Jitendra",
+    secondUsername: "Rakesh"
+  };
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <HelloUser name="Jitendra" />
-        <HelloUser name="Rakesh" />
+        <HelloUser name={this.state.firstUsername} />
+        <HelloUser name={this.state.secondUsername} />
       </div>
     );
   }
